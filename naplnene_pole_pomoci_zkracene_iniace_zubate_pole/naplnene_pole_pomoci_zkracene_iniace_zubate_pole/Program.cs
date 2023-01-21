64f2a88c15f597336d507ebd20608285cada469b
Divisek <113785426+Divisek@users.noreply.github.com>
2023-01-21 14:13:41 +0100

    Add files via upload

---
+++ naplnene_pole_pomoci_zkracene_iniace_zubate_pole/naplnene_pole_pomoci_zkracene_iniace_zubate_pole/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+int[][] zubatePole = new int[][]
+{
+new int []{15,2,8,5,3},
+new int[]{3,3,7},
+new int[]{9,1,16,13},
+new int[]{},
+new int[]{5},
+};
+
+
+
+
+
+
+
